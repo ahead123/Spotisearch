9b8baae5c062a245173e36ab44157b2af413f098
Arthur Head <arthurallenhead@yahoo.com>
2015-04-11 05:08:29 -0700

    made front end look better, and returned artist images from json

--- app.js
+++ app.js
@@ -3,21 +3,28 @@ $(document).ready(function() {
 var $queryBox;
 var $query;
 var id;
+var img_url;
 var baseUrl = "https://api.spotify.com";
 
 // allow users to search for songs, artist, categories on spotify
 // connect to spotify api
 var getResults = function() {
+	$('#results').html("");
 	$queryBox = $('#queryBox');
 	$query = $queryBox.val();
-
+	$('div.intro').hide();
+	$('div.main').show();
+	$('div.left').show();
 	$.getJSON(baseUrl+"/v1/search?q="+$query+"&type=artist", function(json) {
+		$('h2#resultHeader').html("Results For " + $query.toUpperCase());
 		for(var i = 0; i < json.artists.items.length; i++) {
 			id = json.artists.items[i].id;
+			img_url = json.artists.items[i].images[0].url;
+			console.log(json);
 			console.log(id);
-			$('#results').append('<div class="col-md-4"><img class="thumbnail center-block" src=""></div>');
-		}
-		console.log(json);
+			console.log(img_url);
+			$('#results').append('<div class="col-md-2"><a href=""><img class="thumbnail center-block img-responsive" src="'+img_url+'"></a></div>');
+		}			
 	});
 }
 
