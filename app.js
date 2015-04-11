a0c9c0e3fa1317bff0f42fa906caf5b1832457b5
Arthur Head <arthurallenhead@yahoo.com>
2015-04-11 02:21:07 -0700

    initial commit - added navbar and nav styles

---
+++ app.js
@@ -0,0 +1,5 @@
+// allow users to search for songs, artist, categories on spotify
+// connect to spotify api
+//display the search results on the page
+// allow users to add results to favorites
+// allow users to add song url's to a playlist
