f083269e9655cee9ed63e843ebc2c9127bd717f6
Arthur Head <arthurallenhead@yahoo.com>
2015-04-11 13:03:33 -0700

    refactored some jquery to vanilla javascript

--- app.js
+++ app.js
@@ -9,12 +9,23 @@ var baseUrl = "https://api.spotify.com";
 // allow users to search for songs, artist, categories on spotify
 // connect to spotify api
 var getResults = function() {
-	$('#results').html("");
-	$queryBox = $('#queryBox');
-	$query = $queryBox.val();
-	$('div.intro').hide();
-	$('div.main').show();
-	$('div.left').show();
+
+	document.getElementById('results').innerHTML = "";
+	//$('#results').html("");
+
+	$queryBox = document.getElementById('queryBox');
+	//$queryBox = $('#queryBox');
+
+	$query = $queryBox.value;
+	//$query = $queryBox.val();
+
+	document.getElementsByClassName('intro')[0].style.display = "none";
+	//$('div.intro').hide();
+	document.getElementsByClassName('main')[0].style.display = "block";
+	//$('div.main').show();
+	document.getElementsByClassName('left')[0].style.display = "block";
+	//$('div.left').show();
+
 	$.getJSON(baseUrl+"/v1/search?q="+$query+"&type=artist", function(json) {
 		$('h2#resultHeader').html("Results For " + $query.toUpperCase());
 		for(var i = 0; i < json.artists.items.length; i++) {
@@ -23,7 +34,7 @@ var getResults = function() {
 			console.log(json);
 			console.log(id);
 			console.log(img_url);
-			$('#results').append('<div class="col-md-2"><a href=""><img class="thumbnail center-block img-responsive" src="'+img_url+'"></a></div>');
+			$('#results').append('<div class="col-md-4"><a href="https://open.spotify.com/artist/'+id+'"><img class="thumbnail center-block img-responsive" src="'+img_url+'"></a></div>');
 		}			
 	});
 }
@@ -32,7 +43,9 @@ var getResults = function() {
 //display the search results on the page
 // allow users to add results to favorites
 // allow users to add song url's to a playlist
-$('#submitSearch').on('click', getResults); 
+
+document.getElementById('submitSearch').addEventListener('click', getResults);
+//$('#submitSearch').on('click', getResults); 
 
 });
 
