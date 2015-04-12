efed9c661439c08b8895f2485469585dc9130c2b
Arthur Head <arthurallenhead@yahoo.com>
2015-04-11 19:34:58 -0700

    added gh-pages branch

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var short_album_name;
 var album_id;
 var album_url;
 var album_img;
+var favs = [];
 var baseUrl = "https://api.spotify.com";
 
 // allow users to search for songs, artist, categories on spotify
@@ -47,7 +48,7 @@ var getResults = function() {
 				album_id = json.albums.items[x].id;
 				album_img = json.albums.items[x].images[0].url;
 				//$('#results').append('<div class="col-md-4"><a href="https://play.spotify.com/album/'+album_id+'"><img class="thumbnail center-block img-responsive" src="'+album_img+'"></a><p>album title: '+album_name+'</p><p>artist name: '+artist_name+'</p></div>');
-				$('#results').append('<div class="col-md-4"><div class="panel panel-default"><div class="panel-heading"><p class="panel-title pull-left">'+album_name+'</p><a href="https://play.spotify.com/album/'+album_id+'"><p class="text-right tracklist">Open in Spotify</p></a></div><div class="panel-body"><a href="https://play.spotify.com/album/'+album_id+'"><img class="thumbnail center-block img-responsive" src="'+album_img+'"></a></div><div class="panel-footer"><p class="pull-left">'+artist_name+'</p><p class="text-right">Add to Favorites</p></div></div></div>');
+				$('#results').append('<div class="col-md-4"><div class="panel panel-default"><div class="panel-heading"><p class="panel-title pull-left">'+album_name+'</p><a href="https://play.spotify.com/album/'+album_id+'"><p class="text-right tracklist">Open in Spotify</p></a></div><div class="panel-body"><a href="https://play.spotify.com/album/'+album_id+'"><img class="thumbnail center-block img-responsive" src="'+album_img+'"></a></div><div class="panel-footer"><p class="pull-left">'+artist_name+'</p><a class="favTrigger" href=""><p class="text-right">Add to Favorites (coming soon)</p></a></div></div></div>');
 				$('img.artistImage').attr('src', img_url);
 				$('a.albumLink').attr('href', 'https://open.spotify.com/artist/'+id+'');
 			}
