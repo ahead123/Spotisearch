06020b5890792d5b45b97efd27b00191dd3751db
Arthur Head <ahead747@gmail.com>
2015-07-03 12:54:33 -0500

    pull result append out of for loop

--- app.js
+++ app.js
@@ -29,17 +29,17 @@ var getResults = function() {
 			artist_name = json.artists.items[i].name;
 
 			for(var x = 0; x < json.albums.items.length; x++) {
-
+				
 				//album_url = json.albums.items[i].images[x].url;
 				album_name = json.albums.items[x].name;
 				album_id = json.albums.items[x].id;
 				album_img = json.albums.items[x].images[0].url;
-
+				
 				//display the search results on the page
 				//$('#results').append('<div class="col-md-4"><a href="https://play.spotify.com/album/'+album_id+'"><img class="thumbnail center-block img-responsive" src="'+album_img+'"></a><p>album title: '+album_name+'</p><p>artist name: '+artist_name+'</p></div>');
-				$('#results').append('<div class="col-md-4"><div class="panel panel-default"><div class="panel-heading"><p class="panel-title pull-left">'+album_name+'</p><a href="https://play.spotify.com/album/'+album_id+'"><p class="text-right tracklist">Open in Spotify</p></a></div><div class="panel-body"><a href="https://play.spotify.com/album/'+album_id+'"><img class="thumbnail center-block img-responsive" src="'+album_img+'"></a></div><div class="panel-footer"><p class="pull-left">'+artist_name+'</p><a class="favTrigger" href="#" onclick="pushFav()"><p class="text-right">Add to Favorites (coming soon)</p></a></div></div></div>');
+				//$('#results').append('<div class="col-md-4"><div class="panel panel-default"><div class="panel-heading"><p class="panel-title pull-left">'+album_name+'</p><a href="https://play.spotify.com/album/'+album_id+'"><p class="text-right tracklist">Open in Spotify</p></a></div><div class="panel-body"><a href="https://play.spotify.com/album/'+album_id+'"><img class="thumbnail center-block img-responsive" src="'+album_img+'"></a></div><div class="panel-footer"><p class="pull-left">'+artist_name+'</p><a class="favTrigger" href="#" onclick="pushFav()"><p class="text-right">Add to Favorites (coming soon)</p></a></div></div></div>');
 				$('a.albumLink').attr('href', 'https://open.spotify.com/artist/'+id+'');
-
+				
 			} // ends album loop
 
 		} // ends artists loop
@@ -69,7 +69,7 @@ var getResults = function() {
 
   // checks to see if search term is in cache. if so - stops api call and pulls data from cache object.
 	if($query in cache) {
-	  console.log('hit cache', $query, cache[$query]);
+	  //console.log('hit cache', $query, cache[$query]);
 	  render(cache[$query]);
 	  return false;
 	} 
@@ -86,8 +86,8 @@ var getResults = function() {
 $('#submitSearch').click(getResults); 
 
 // also fires getResults function on the enter keypress event
-$('input#queryBox').on('keypress', function (e) {
-    if (e.which == 13) {
+$('input#queryBox').on('keyup', function (e) {
+    if(e.which === 13) {
       getResults();
     }
  });
